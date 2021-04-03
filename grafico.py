960d27107d11ad46e1cec5c977fe5c2eb06958fc
Christopher Tineo <1099919@est.intec.edu.do>
2021-04-02 20:54:47 -0400

    #6 Calcular el total de los montos por categorías

--- grafico.py
+++ grafico.py
@@ -1,2 +1,31 @@
-﻿def generar():
-    print("Funciona")
+﻿def calcularTotales():
+
+    totalCategorias = {
+        "Comida": 0,
+        "Entretenimiento": 0,
+        "Servicios": 0,
+        "Ropa": 0,
+        "Sueldo": 0,
+        "Prestamo": 0,
+        "Venta": 0
+    }
+
+    with open("historial.txt", "r") as historial:
+        lines = historial.readlines()
+
+    i = 0
+
+    for line in lines:
+        line = line.rstrip('\n')
+        if line in totalCategorias.keys():
+            totalCategorias[line] = int(lines[i+1].rstrip('\n'))
+        i += 1
+
+    return totalCategorias
+
+
+def generar():
+    total = calcularTotales()
+
+
+generar()
